3666285f81e2684b8567f41e653130379f8b0541
svitlana-p <sweetlana.pukish@gmail.com>
2020-06-12 22:21:53 +0300

    added forms

--- js/index.js
+++ js/index.js
@@ -28,3 +28,47 @@ function icoPressed() {
     const ico3 = document.querySelector('.ico3');
     ico3.src = icoArray[curIcoInd];
 }
+window.addEventListener('DOMContentLoaded', function () {
+  // get the form elements defined in your form HTML above
+
+  const form = document.getElementById('my-form');
+  const button = document.getElementById('my-form-button');
+  const status = document.getElementById('my-form-status');
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    button.style = 'display: none ';
+    status.innerHTML = 'Thanks!';
+  }
+
+  function error() {
+    status.innerHTML = 'Oops! There was a problem.';
+  }
+
+  // handle the form submission event
+
+  form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    const data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  const xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
